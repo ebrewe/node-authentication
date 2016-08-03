408062be7528e9e5fc926017b309016d0e96ffd1
Eric Brewer <eric.brewer@sandboxww.com>
2016-08-03 10:37:11 -0400

    finished user model

--- server/models/user.js
+++ server/models/user.js
@@ -2,3 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //define model
+const userSchema = new Schema({
+  email: {type:String, unique:true, lowercase:true},
+  password: String
+});
+
+
+//create model class
+const Model = mongoose.model('user', userSchema);
+
+//export the model
+module.exports = Model;
